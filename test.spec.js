a7357dd1bf1aeced207501729021ae2575a468bf
Filimon Kiros <filimona.kiros@gmail.com>
2023-04-18 18:02:14 -0400

    final code clean up

--- test.spec.js
+++ test.spec.js
@@ -1,18 +1,30 @@
 const textProcessor_JestTest = (inputString) => {
+  /*
+  I- string (words/sentences)
+  O- key:value pair of letter and frequency. In app, the return value should be used to update state variables
+  E- no input? no alphabets in input?
+  C- Time Complexity:
+        Functions: toLowerCase (linear), filter (linear), enteries (constant), sort O(nlog n) (depends on runtime engine: Chrome uses mergeSort/insertionSort, Firefox uses mergeSort)
+        Approx worst case time complexity: O(2n) + O(1) + O(nlog n)
+     Space Complexity:
+        Functions: split (linear).
+        Objects: frequencyDictonary (constant, as max is 26 slots for alphabets)
+        Approx worse case space complexcity: O(n) + O(1)
+  */
+
   if (!inputString) {
-    // handle edge case when user leaves text area blank
-    return "Empty input";
+    return "Empty input"; // handle edge case when user leaves text area blank
   } else {
     let filteredLetters = inputString
       .toLowerCase()
       .split("")
       .filter((char) => /[a-z]/.test(char)); // filter out non alphabet chars
 
-    let frequencyDictonary = {};
-
     if (filteredLetters.length === 0) {
       return "No english letters found in input";
     }
+
+    let frequencyDictonary = {};
 
     filteredLetters.forEach((char) => {
       !frequencyDictonary[char]
